9341b0257db706d84a595ce17133c547b23e9637
Jennifer Cano <jcano2@oxy.edu>
2021-02-07 03:58:47 -0800

    "Finished Activity 2"

--- app/src/main/java/com/example/homework1/SecondActivity.java
+++ app/src/main/java/com/example/homework1/SecondActivity.java
@@ -8,9 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class SecondActivity extends AppCompatActivity {
     private Button button_generate;
     private String receivedMessage;
     private String receivedMessage1;
+    private String receivedMessage2;
 
 
     @Override
@@ -35,25 +41,56 @@ public class SecondActivity extends AppCompatActivity {
         linearLayout = findViewById(R.id.first_linear_layout);
         button_generate = findViewById(R.id.button_generate);
 
+        List<EditText> allUserinputs = new ArrayList<>();
+
         for(int i = 0; i < finalresult.length; i++) {
+            finalresult[i] = finalresult[i].replace("\"", "");
+            finalresult[i] = finalresult[i].replace("[", "");
+            finalresult[i] = finalresult[i].replace("]", "");
             EditText editText = new EditText(this);
             TextView textView = new TextView(this);
             textView.setText(finalresult[i]);
             linearLayout.addView(editText);
             linearLayout.addView(textView);
+            allUserinputs.add(editText);
         }
 
         button_generate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
+
+                String[] strings = new String[allUserinputs.size()];
+
+
+                for(int i=0; i < allUserinputs.size(); i++){
+                    strings[i] = allUserinputs.get(i).getText().toString();
+                }
+
+                for(int i=0; i < strings.length; i++){
+                    if(strings[i].matches("") ){
+                        sayError(v);
+                        return;
+                    }
+                }
+
+                Log.d("info", Arrays.toString(strings));
+                Bundle b = new Bundle();
+                b.putStringArray("info", strings);
+                receivedMessage2 = intent.getStringExtra("info");
                 receivedMessage1 = intent.getStringExtra("value");
                 launchNextActivity(v);
             }
         });
     }
-    private void launchNextActivity(View view) {
 
+    public void sayError(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void launchNextActivity(View view) {
         Intent intent = new Intent(this, ThirdActivity.class);
+        intent.putExtra("value", receivedMessage2);
         intent.putExtra("value", receivedMessage1);
         startActivity(intent);
 
--- app/src/main/java/com/example/homework1/ThirdActivity.java
+++ app/src/main/java/com/example/homework1/ThirdActivity.java
@@ -2,6 +2,7 @@ package com.example.homework1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -15,6 +16,7 @@ public class ThirdActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private Button button_gohome;
     private String receivedMessage1;
+    private String receivedMessage2;
 
 
     @Override
@@ -30,6 +32,12 @@ public class ThirdActivity extends AppCompatActivity {
         TextView textView = new TextView(this);
         textView.setText(receivedMessage1);
         linearLayout.addView(textView);
+
+        receivedMessage2 = intent.getStringExtra("info");
+        TextView textView1 = new TextView(this);
+        Log.d("info","Val " + String.valueOf(receivedMessage2));
+        textView1.setText(String.valueOf(receivedMessage2));
+        linearLayout.addView(textView1);
 
 
         button_gohome.setOnClickListener(new View.OnClickListener() {
